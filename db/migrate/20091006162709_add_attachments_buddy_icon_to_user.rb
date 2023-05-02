b32b834d5e6edb6ead1ef2bfa40e3d3b8b81cfdd
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-05-02 18:51:23 +0500

    removed migration

--- db/migrate/20091006162709_add_attachments_buddy_icon_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddAttachmentsBuddyIconToUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :buddy_icon_file_name
-    remove_column :users, :buddy_icon_content_type
-    remove_column :users, :buddy_icon_file_size
-    remove_column :users, :buddy_icon_updated_at
-  end
-end
