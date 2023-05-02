ef2f6cfc0f2522092a6e96771e4b8394df565881
Taimoor Hassan <taimoor.hassan10@yahoo.com>
2023-05-02 18:51:40 +0500

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2009_10_11_210447) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_06_183704) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "activities", force: :cascade do |t|
     t.integer "user_id"
     t.integer "partner_id"
@@ -960,8 +988,6 @@ ActiveRecord::Schema[7.0].define(version: 2009_10_11_210447) do
     t.string "salt", limit: 40
     t.string "first_name", limit: 100
     t.string "last_name", limit: 100
-    t.datetime "created_at"
-    t.datetime "updated_at"
     t.datetime "activated_at"
     t.string "activation_code", limit: 60
     t.string "remember_token", limit: 60
@@ -1056,6 +1082,8 @@ ActiveRecord::Schema[7.0].define(version: 2009_10_11_210447) do
     t.boolean "is_branch_chosen", default: false
     t.boolean "is_importing_contacts", default: false
     t.integer "imported_contacts_count", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["facebook_uid"], name: "index_users_on_facebook_uid"
     t.index ["rss_code"], name: "index_users_on_rss_code"
     t.index ["status"], name: "index_users_on_status"
@@ -1072,4 +1100,6 @@ ActiveRecord::Schema[7.0].define(version: 2009_10_11_210447) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
