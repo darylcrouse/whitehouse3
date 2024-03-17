a3f6eef351e2c319c7c6e0f666b4d683f3b4d0e9
darylcrouse <darylcrouse@gmail.com>
2024-03-17 12:22:43 -0700

    Update about_controller.rb

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,29 +1,32 @@
 class AboutController < ApplicationController
-  
   def index
-    @page_title = t('about.index', :government_name => current_government.name)
+    @page_title = t('about.index', government_name: current_government.name)
   end
-  
+
   def show
-    if params[:id] == 'privacy'
-      @page_title = t('about.privacy', :government_name => current_government.name)       
-      render :action => "privacy"
-    elsif params[:id] == 'rules'
-      @page_title = t('about.rules', :government_name => current_government.name)     
-      render :action => "rules"
-    elsif params[:id] == 'faq'
-      @page_title = t('about.faq', :government_name => current_government.name)      
-      render :action => "faq"
-    elsif params[:id] == 'stimulus'
-      @page_title = "How America rates the stimulus package"
-      render :action => "stimulus"      
-    elsif params[:id] == 'congress'
-      redirect_to "http://hellocongress.org/"
-      return
+    case params[:id]
+    when 'privacy'
+      @page_title = t('about.privacy', government_name: current_government.name)
+      render action: 'privacy'
+    when 'rules'
+      @page_title = t('about.rules', government_name: current_government.name)
+      render action: 'rules'
+    when 'faq'
+      @page_title = t('about.faq', government_name: current_government.name)
+      render action: 'faq'
+    when 'stimulus'
+      @page_title = 'How America rates the stimulus package'
+      render action: 'stimulus'
+    when 'congress'
+      redirect_to 'http://hellocongress.org/'
     else
-      @page = Page.find_by_short_name(params[:id])
-      @page_title = @page.name
+      @page = Page.find_by(short_name: params[:id])
+      if @page.present?
+        @page_title = @page.name
+        render action: 'show'
+      else
+        raise ActiveRecord::RecordNotFound
+      end
     end
   end
-  
 end
