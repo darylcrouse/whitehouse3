4ee0bf51805ab7edf7456e3c661890fc6c390a6b
darylcrouse <darylcrouse@gmail.com>
2024-03-17 12:24:07 -0700

    Update activities_controller.rb

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,82 +1,74 @@
 class ActivitiesController < ApplicationController
-  
-  before_filter :admin_required, :only => [:edit, :update]
-  before_filter :login_required, :only => [:destroy, :undelete]
-  
+  before_action :admin_required, only: [:edit, :update]
+  before_action :login_required, only: [:destroy, :undelete]
+
   def index
     if request.format != 'html'
-      @activities = Activity.active.by_recently_created.paginate :page => params[:page], :per_page => params[:per_page]
+      @activities = Activity.active.by_recently_created.page(params[:page]).per(params[:per_page])
     end
+    
     respond_to do |format|
-      format.html { redirect_to :controller => "news", :action => "activities" } # redirect to all activity
-      format.xml { render :xml => @activities.to_xml(:include => [:user, :comments], :except => NB_CONFIG['api_exclude_fields']) }
-      format.json { render :json => @activities.to_json(:include => [:user, :comments], :except => NB_CONFIG['api_exclude_fields']) }      
-    end    
+      format.html { redirect_to news_activities_path }
+      format.xml { render xml: @activities.to_xml(include: [:user, :comments], except: NB_CONFIG['api_exclude_fields']) }
+      format.json { render json: @activities.to_json(include: [:user, :comments], except: NB_CONFIG['api_exclude_fields']) }
+    end
   end
 
-  # GET /activities/1
-  # GET /activities/1.xml
   def show
     @activity = Activity.find(params[:id])
+    
     respond_to do |format|
-      format.html { redirect_to activity_comments_url(@activity) }
-      format.xml { render :xml => @activity.to_xml(:include => :user, :except => NB_CONFIG['api_exclude_fields']) }
-      format.json { render :json => @activity.to_json(:include => :user, :except => NB_CONFIG['api_exclude_fields']) }      
-    end    
-  end
-  
-  #GET /activities/1/unhide
-  def unhide
-    @activity = Activity.find(params[:id])    
-    respond_to do |format|
-      format.js {
-        render :update do |page|
-          page.replace 'activity_and_comments_' + @activity.id.to_s, render(:partial => "activities/show", :locals => {:activity => @activity, :suffix => ""})
-        end
-      }
+      format.html { redirect_to activity_comments_path(@activity) }
+      format.xml { render xml: @activity.to_xml(include: :user, except: NB_CONFIG['api_exclude_fields']) }
+      format.json { render json: @activity.to_json(include: :user, except: NB_CONFIG['api_exclude_fields']) }
     end
   end
 
-  # PUT /activities/1
-  # PUT /activities/1.xml
+  def unhide
+    @activity = Activity.find(params[:id])
+    
+    respond_to do |format|
+      format.js { render 'unhide', locals: { activity: @activity } }
+    end
+  end
+
   def update
     @activity = Activity.find(params[:id])
 
     respond_to do |format|
-      if @activity.update_attributes(params[:activity])
+      if @activity.update(activity_params)
         flash[:notice] = 'Activity was successfully updated.'
-        format.html { redirect_to(@activity) }
+        format.html { redirect_to @activity }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
+        format.html { render :edit }
       end
     end
   end
 
-  # DELETE /activities/1
-  # DELETE /activities/1.xml
-
   def destroy
     @activity = Activity.find(params[:id])
-    access_denied unless current_user.is_admin? or @activity.user_id == current_user.id
+    access_denied unless current_user.is_admin? || @activity.user_id == current_user.id
     @activity.delete!
+    
     respond_to do |format|
-      format.html { redirect_to(activities_url) }
-      format.js {
-        render :update do |page|
-          page.remove 'activity_and_comments_' + @activity.id.to_s
-        end        
-      }
+      format.html { redirect_to activities_path }
+      format.js { render 'destroy', locals: { activity: @activity } }
     end
   end
 
-  # PUT /activities/1/undelete
   def undelete
     @activity = Activity.find(params[:id])
     @activity.undelete!
+    
     respond_to do |format|
-      format.html { redirect_to(activities_url) }
+      format.html { redirect_to activities_path }
     end
   end
 
+  private
+
+  def activity_params
+    params.require(:activity).permit(:your_permitted_attributes)
+  end
 end
