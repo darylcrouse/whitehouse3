f870f5a6779a9d90598687d819a004a2961f09f6
darylcrouse <darylcrouse@gmail.com>
2024-03-17 12:11:42 -0700

    Update notifications_controller.rb

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,33 +1,31 @@
 class NotificationsController < ApplicationController
+  before_action :login_required
+  before_action :set_notification, only: [:show, :destroy]
 
-  before_filter :login_required
-  
   def authorized?
-    @notification = Notification.find(params[:id])
-    current_user.is_admin? or @notification.recipient_id == current_user.id
+    current_user.is_admin? || @notification.recipient_id == current_user.id
   end
 
-  # GET /notifications/1
-  # GET /notifications/1.xml
   def show
     respond_to do |format|
       format.html
-      format.xml { render :xml => @notification.to_xml(:include => [:sender, :notifiable], :except => NB_CONFIG['api_exclude_fields']) }
-      format.json { render :json => @notification.to_json(:include => [:sender, :notifiable], :except => NB_CONFIG['api_exclude_fields']) }
+      format.xml { render xml: @notification.to_xml(include: [:sender, :notifiable], except: NB_CONFIG['api_exclude_fields']) }
+      format.json { render json: @notification.to_json(include: [:sender, :notifiable], except: NB_CONFIG['api_exclude_fields']) }
     end
   end
 
-  # DELETE /notifications/1
-  # DELETE /notifications/1.xml
   def destroy
     @notification.delete!
     respond_to do |format|
-      format.html { redirect_to(:controller => "inbox", :action => "notifications") }
+      format.html { redirect_to(controller: "inbox", action: "notifications") }
       format.js {
-        render :update do |page|
-          page.remove 'notification_' + @notification.id.to_s
-        end        
+        render js: "document.getElementById('notification_#{@notification.id}').remove();"
       }
     end
   end
+
+  private
+    def set_notification
+      @notification = Notification.find(params[:id])
+    end
 end
